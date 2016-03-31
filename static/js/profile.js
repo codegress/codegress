b1dc399236053683f319c0d718afa9c520ab1894
tejaswiMinnu <tjminnu@github.com>
2016-03-31 17:55:16 +0530

    Custom Profile Page

--- static/js/profile.js
+++ static/js/profile.js
@@ -8,3 +8,35 @@ function actualInit(apiRoot){
   apisToLoad = 1;
   gapi.client.load('codegress', 'v1', callback, apiRoot); 
 };
+
+$('.pencil').mouseenter(function(){
+	$(this).children('.glyphicon-pencil').removeClass('hide');
+	});
+
+$('.pencil').mouseleave(function(){
+	$(this).children('.glyphicon-pencil').addClass('hide');
+	});
+
+$('.fullname-pencil > .glyphicon-pencil').click(function(){
+	 $('.fullname-pencil').addClass('hide');
+	 var fullname = $('#fullname').text();
+	 $('#fullname-edit').val(fullname);
+	 $('.fullname-change').removeClass('hide');
+
+});
+
+$('.email-pencil > .glyphicon-pencil').click(function(){
+	 $('.email-pencil').addClass('hide');
+	 var email = $('#email').text();
+	 $('#email-edit').val(email);
+	 $('.email-change').removeClass('hide');
+
+});
+
+$('.country-pencil > .glyphicon-pencil').click(function(){
+	 $('.country-pencil').addClass('hide');
+	 var country = $('#country').text();
+	 $('#country-edit').val(country);
+	 $('.country-change').removeClass('hide');
+
+});
